5305a8f9e564f8d16f2153c086f5d2a10f1151f2
Sebastian200Gutierrez <sebastian.gutierrez03@unicatolica.edu.co>
2025-06-25 20:21:32 -0500

    Primer commit del backend

---
+++ main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+
+app = FastAPI()
+
+#--------------------------------------------------------------
+@app.get("/")
+def read_root():
+    return {"mensaje": "¡Hola desde FastAPI!"}
+
+#--------------------------------------------------------------
+@app.get("/saludo/{nombre}")
+def saludar(nombre: str):
+    return {"saludo": f"Hola, {nombre}!"}
+
+#--------------------------------------------------------------
+@app.post("/enviar/{dato}")
+def recibir_dato(dato: str):
+    return {"respuesta": f"Me has enviado: {dato}"}
+
+#--------------------------------------------------------------
+class Persona(BaseModel):
+    nombre: str
+    cedula: str
+
+@app.post("/registrar")
+def registrar_persona(persona: Persona):
+    return {
+        "nombre_recibido": persona.nombre,
+        "cedula_recibida": persona.cedula
+    }
+
+#--------------------------------------------------------------
+class Numeros(BaseModel):
+    lista: List[int]  # 👈 Aquí se asegura que todos los datos sean enteros
+
+@app.post("/analizar-numeros")
+def analizar_numeros(numeros: Numeros):
+    datos = numeros.lista
+    if not datos:
+        return {"error": "La lista está vacía"}
+
+    mayor = max(datos)
+    menor = min(datos)
+    promedio = sum(datos) / len(datos)
+
+    return {
+        "mayor": mayor,
+        "menor": menor,
+        "promedio": round(promedio, 2)
+    }
